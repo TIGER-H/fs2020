ff76d2ebd4785e65a4527965b1ae32ce6c06ea91
yeoreum <549747168@qq.com>
2021-03-17 11:18:39 +0800

    part2 2-3notes & 2.6 init

--- classnote/src/App.js
+++ classnote/src/App.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState("a new note...")
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll?notes:notes.filter(note=>note.important)
+
+  const addNote = (event) => {
+    event.preventDefault() 
+    //阻止提交表单的默认操作。 因为默认操作会导致页面重新加载。
+    // console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote("NEW ONE?!")
+  }
+
+  const handleNoteChange = (event) => {
+    // console.log(event.target) => <input value="a new note..">
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={()=> setShowAll(!showAll)}>
+          show {showAll?'important':'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
             <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
