3dc8c1888adc8272fa5da89888dcf6574ae15160
yeoreum <549747168@qq.com>
2021-03-19 15:02:34 +0800

    part2-6

--- classnote/src/App.js
+++ classnote/src/App.js
@@ -1,27 +1,38 @@
-import React, { useState, useEffect} from 'react'
-import Note from './components/Note'
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState("a new note...")
-  const [showAll, setShowAll] = useState(true)
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
-  const notesToShow = showAll?notes:notes.filter(note=>note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server!`);
+        setNotes(notes.filter((note) => note.id !== id));
+      });
+  };
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+  // console.log(/"render", notes.length, "notes");
 
   const addNote = (event) => {
-    event.preventDefault() 
+    event.preventDefault();
     //阻止提交表单的默认操作。 因为默认操作会导致页面重新加载。
     // console.log('button clicked', event.target)
     const noteObject = {
@@ -29,37 +40,43 @@ const App = () => {
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    }
+    };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote("NEW ONE?!")
-  }
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
 
   const handleNoteChange = (event) => {
     // console.log(event.target) => <input value="a new note..">
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
-        <button onClick={()=> setShowAll(!showAll)}>
-          show {showAll?'important':'all'}
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
-        )}
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
