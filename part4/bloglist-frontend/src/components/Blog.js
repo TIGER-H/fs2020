4fe90cd92625a3df80469b132401e1812a7099fe
yeoreum <549747168@qq.com>
2021-04-18 00:49:00 +0800

    5.7 with a fake like button

--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visibility, setVisibility] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const addLike = () => {
+
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisibility(!visibility)}>
+          {visibility ? 'hide' : 'show'}
+        </button>
+      </div>
+      <div>
+        {visibility && <div>
+          {blog.url} <br/>
+          {blog.likes} 
+          <button onClick={addLike}>like</button> <br/>
+          {blog.user.name}
+        </div> }
+      </div>
+    </div>
+  )
+}
 
 export default Blog
