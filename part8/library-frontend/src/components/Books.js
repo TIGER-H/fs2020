f61a982be3add2ea0fece6300049fa574cb8549e
yeoreum <549747168@qq.com>
2021-05-13 22:07:38 +0800

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('all')
+
   if (!props.show) {
     return null
   }
 
   const books = props.books.data.allBooks
+  console.log(books)
+
+  let allGenres = []
+  books.forEach((book) => {
+    allGenres = [
+      ...allGenres,
+      ...book.genres.filter((genre) => !allGenres.includes(genre)),
+    ]
+  })
+  // console.log(allGenres)
 
   return (
     <div>
@@ -14,23 +26,32 @@ const Books = (props) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <td>
+              in genre <b>{genre}</b>
+            </td>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) =>
+            a.genres.includes(genre) || genre === 'all' ? (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ) : null
           )}
         </tbody>
       </table>
+      {allGenres.map((genre) => (
+        <button key={genre} onClick={() => setGenre(genre)}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => setGenre('all')}>all genres</button>
     </div>
   )
 }
