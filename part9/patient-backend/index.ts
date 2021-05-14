a4ae0f4b5f44fb46c7c7acfb7e4a8365aaf55c8d
yeoreum <549747168@qq.com>
2021-05-14 19:25:17 +0800

    9.9

---
+++ part9/patient-backend/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+    res.send('good')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
