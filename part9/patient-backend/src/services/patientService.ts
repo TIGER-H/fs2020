51041c13ea4cd54825926f95719b6c05eed40a26
yeoreum <549747168@qq.com>
2021-05-20 16:50:08 +0800

    9.19

--- part9/patient-backend/src/services/patientService.ts
+++ part9/patient-backend/src/services/patientService.ts
@@ -1,16 +1,17 @@
-import patientsData from '../data/patients.json';
+import patientsData from '../data/patients';
 import { newPatientEntry, Patient, PublicPatient } from '../types';
 import { v1 as uuid } from 'uuid';
 
 const patientsPublic: PublicPatient[] = patientsData;
+const patients: Patient[] = patientsData
 
 const getEntries = (): PublicPatient[] => {
     return patientsPublic.map(({ id, name, dateOfBirth, gender, occupation }) => ({ id, name, dateOfBirth, gender, occupation })
     );
 };
 
-const getEntry = (id: string): PublicPatient | undefined => {
-    const foundPatient = patientsPublic.find(patient => patient.id === id);
+const getEntry = (id: string): Patient | undefined => {
+    const foundPatient = patients.find(patient => patient.id === id);
     if (!foundPatient) throw new Error('not found');
     return foundPatient;
 };
