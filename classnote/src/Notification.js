ab1c3e2a84a377815bdb59511b61edfe498c3445
yeoreum <549747168@qq.com>
2021-05-23 22:52:35 +0800

    delete notes

--- classnote/src/Notification.js
+++
@@ -1,6 +0,0 @@
-const NotificationMessage = ({ message }) => {
-    if (message === null) return null;
-    return <div className="error">{message}</div>;
-  };
-
-export default NotificationMessage
