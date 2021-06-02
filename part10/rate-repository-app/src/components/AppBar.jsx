15410b4bf4b8806d7aa4111c66449effb3be3569
yeoreum <549747168@qq.com>
2021-06-02 23:33:48 +0800

    10.7

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: theme.colors.appBarBackground,
-        flexDirection:'row'
-    },
-    text: {
-        color: theme.colors.appBarText,
-        padding: 14,
-    }
-    // ...
+  container: {
+    display: 'flex',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.appBarBackground,
+    flexDirection: 'row',
+  },
+  text: {
+    color: theme.colors.appBarText,
+    padding: 14,
+  },
+  // ...
 });
 
 const AppBar = () => {
-    return <View style={styles.container}>
-        <AppBarTab name={"Repositories"} url='/' />
-        <AppBarTab name={"Sign in"} url='/signin' />
-    </View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <AppBarTab name={'Repositories'} url='/' />
+        <AppBarTab name={'Sign in'} url='/signin' />
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBar;
